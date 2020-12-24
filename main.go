334a399642e47670f71c9feb3c8edfb5bb06635e
Imad Jerra <imad.jerra@ariv.ca>
2020-12-24 10:40:46 -0500

    Json encode / decode

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -16,9 +18,22 @@ func main() {
 }
 
 func encode(w http.ResponseWriter, r *http.Request) {
+	p := person{First: "Jenny"}
 
+	encoder := json.NewEncoder(w)
+	err := encoder.Encode(p)
+	if err != nil {
+		http.Error(w, "error encoding json", http.StatusInternalServerError)
+	}
 }
 
 func decode(w http.ResponseWriter, r *http.Request) {
+	var p person
 
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		http.Error(w, "error decoding json", http.StatusInternalServerError)
+	}
+
+	log.Println("Person: ", p)
 }
